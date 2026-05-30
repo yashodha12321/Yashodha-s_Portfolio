c94e6a58e5d5e0c2354fb290ce7ef01970cd9699
yashodha12321 <haarikayashodha35@gmail.com>
2026-05-30 21:20:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function AWS() {
+    window.open("AWS.jpeg", "_blank");
+}
+function CODZEN() {
+    window.open("CODZEN.jpeg", "_blank");
+}
+
+function DEVOPS() {
+    window.open("DEVOPS.jpeg", "_blank");
+}
+function WADHWANI() {
+    window.open("WADHWANI.jpeg", "_blank");
+}
